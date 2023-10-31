2aace8becc4a6a849dc2d0cb2270ec700993d36b
yvonnegichovi <yvonnegichovi@gmail.com>
2023-10-31 13:57:16 -0700

    bytecode

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b, c):
+    if a < b:
+        result = c
+    elif c > b:
+        result = a + b
+    else:
+        result = a * b - c
+    return result

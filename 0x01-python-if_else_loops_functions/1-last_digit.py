0a27bafa7dac0ec87cb9306e050cb632a58376c0
yvonnegichovi <yvonnegichovi@gmail.com>
2023-10-31 03:46:54 -0700

    fixing coding style error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,9 +7,11 @@ last_digit = (abs_number % 10)
 if number < 0:
     last_digit = -last_digit
 
+print(f"Last digit of {number} is {last_digit}", end = ' ')
+
 if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print(f"and is greater than 5")
 elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is zero")
+    print(f"and is 0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(f"and is less than 6 and not 0")

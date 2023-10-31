334febba5072f4b64c0ce92c516cc68b7a729301
yvonnegichovi <yvonnegichovi@gmail.com>
2023-10-31 08:27:14 -0700

    uppercase printing

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            uppercase_char = chr(ord(char) - (ord('a') - ord('A')))
+        else:
+            uppercase_char = char
+        print(uppercase_char, end='')
+    print()

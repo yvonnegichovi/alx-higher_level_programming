efcd2cdf86cf4a3018333cb7f0fab78c2827d47a
yvonnegichovi <yvonnegichovi@gmail.com>
2023-11-02 05:01:45 -0700

    importing a module that performs maths related caluclations

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+    result = sub(a, b)
+    print("{} - {} = {}".format(a, b, result))
+    result = mul(a, b)
+    print("{} * {} = {}".format(a, b, result))
+    result = div(a, b)
+    print("{} / {} = {}".format(a, b, result))

362a3fade84db2ec1ffdbb0d8696c3d861cd9e9b
yvonnegichovi <yvonnegichovi@gmail.com>
2023-11-02 04:48:16 -0700

    importing add module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,8 +2,8 @@
 
 from add_0 import add
 
-a = 1
-b = 2
-
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))

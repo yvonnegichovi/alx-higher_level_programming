dd22f2a2a2b2d2bfe44ec6cadeb43d5a146d8355
yvonnegichovi <yvonnegichovi@gmail.com>
2023-11-04 09:34:51 -0700

    returning a number at a certain position in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx <= -1:
+    if idx <= -1 or idx > len(my_list):
         return None
-    elif idx > len(my_list):
-        return None
-    else:
-        return my_list[idx]
+    return my_list[idx]

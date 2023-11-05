4ea24d65c8088a30243e26da1dc805137c62969a
yvonnegichovi <yvonnegichovi@gmail.com>
2023-11-05 03:51:32 -0800

    a function with multiple returns from a tuple

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    result = ""
+    if len(sentence) == 0:
+        sentence[0] == None
+    else:
+        result = len(sentence), sentence[0]
+        return result

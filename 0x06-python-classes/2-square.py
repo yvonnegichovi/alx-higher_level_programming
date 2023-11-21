59e3c0e828cefa21022c445d5c959a895437b093
yvonnegichovi <yvonnegichovi@gmail.com>
2023-11-21 11:20:41 -0800

    class and objects with a combination of try and exception

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""This module defines a square"""
+
+
+class Square:
+    """This class defines the class attribute Square"""
+    def __init__(self, size=0):
+        """The init method is used to initialize the definition of square
+
+        uses Private instance attribute size and try and exception method"""
+        try:
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        except TypeError as te:
+            print(te)
+        except ValueError as ve:
+            print(ve)

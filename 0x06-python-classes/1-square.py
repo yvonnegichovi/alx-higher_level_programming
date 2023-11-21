c0d86e3769a1f2af8a5349328346546dcf77e9ef
yvonnegichovi <yvonnegichovi@gmail.com>
2023-11-21 11:07:16 -0800

    defining the square class that was blank in task 0 using a private instance attribute of size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""This modulewrites a class Square that defines a square"""
+
+
+class Square:
+    """defines a square"""
+    def __init__(self, size):
+        """this init method will use size to define a square
+
+        size is a private variable"""
+        self.__size = size

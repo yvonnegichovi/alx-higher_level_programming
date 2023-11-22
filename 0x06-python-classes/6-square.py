8133d077a6bad6e42f7d0cc54a57bc648f442ea2
yvonnegichovi <yvonnegichovi@gmail.com>
2023-11-22 11:10:05 -0800

    fixing an error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -66,7 +66,8 @@ class Square:
         """This  method prints area in stdout with character #"""
         if self.__size == 0:
             print("")
-        for i in range(self.__position[1]):
-            print("")
-        for i in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
+        else:
+            for i in range(self.__position[1]):
+                print("")
+            for i in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)

344326d8c8623a822f506a7eeb4ccf9940a0362c
yvonnegichovi <yvonnegichovi@gmail.com>
2023-11-22 09:55:44 -0800

    returns area and uses the concept of data encalpsulation to handle private instances

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""This module defines a square"""
+
+
+class Square:
+    """This class defines the fields and methods of a Square class"""
+
+    def __init__(self, size=0):
+        """This method initializes the size of the Square class"""
+        self.size = size
+
+    @property
+    def size(self):
+        """This getter method retrieves the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """This setter method sets the private instance of size
+
+        while considering some conditions"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """This method returns the area of the Square"""
+        return self.__size * self.__size

adc0efbee968829763846da676777aae8d2f20a5
yvonnegichovi <yvonnegichovi@gmail.com>
2023-11-24 14:31:31 -0800

    task 4 py file created

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""This module looks for a couple of characters and add new lines to it"""
+
+
+def text_indentation(text):
+    """This function that prints a text with 2 new lines
+    after each of these characters: ., ? and :"""
+    s = [".", "?", ":"]
+    current_line = ""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    for i in text:
+        current_line += i
+        if i in s:
+            print(current_line.strip())
+            print()
+            current_line = ""
+    if current_line:
+        print(current_line.strip())

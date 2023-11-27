3637ca48d0285d259b7d127785cfae80b262b584
yvonnegichovi <yvonnegichovi@gmail.com>
2023-11-27 05:13:09 -0800

    an empty class Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""This module defines a Square class."""
+
+
+class Rectangle:
+    """An empty class taht defines a square"""
+    def __init__(self):
+        """method: init that initializes a new instance"""
+        pass

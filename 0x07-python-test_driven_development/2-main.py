13d04d0c705993b49d588e920f502382477056fd
yvonnegichovi <yvonnegichovi@gmail.com>
2023-12-01 07:58:15 -0800

    fixing indentation, and error message when two arguments are missing

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
+matrix = NULL
 
-matrix = []
-print(matrix_divided(matrix, 3))
+print(matrix_divided(matrix)
 print(matrix)

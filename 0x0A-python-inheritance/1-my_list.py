ce058a0c3dc0645397efdc9d24b442f1e5efea98
yvonnegichovi <yvonnegichovi@gmail.com>
2023-12-04 08:25:09 -0800

    be

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,4 +7,6 @@ class MyList(list):
     """This class has a list"""
     def print_sorted(self):
         """This method prints the list but in ascending order"""
-        print(sorted(self))
+        sorted_list = sorted(self)
+        filtered_list = [i for i in sorted_list if isinstance(i, int)]
+        print(filtered_list)

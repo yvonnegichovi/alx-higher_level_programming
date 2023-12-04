ccccf73eba226fa45415cdf98d8191556b5623b5
yvonnegichovi <yvonnegichovi@gmail.com>
2023-12-04 11:16:38 -0800

    writing a subclass

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+"""This module creates a class"""
+
+
+class BaseGeometry:
+    """A class instance"""
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Public instance that validates value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """A class that inherits form the class BaseGeometry"""
+    def __init__(self, width, height):
+        """Instantiation of width and height using method init"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__height = height
+        self.__width = width

8ac9cd074347489693bd9ffa225224467a07887c
yvonnegichovi <yvonnegichovi@gmail.com>
2023-12-04 08:27:01 -0800

    fixing errors

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,4 @@ class MyList(list):
     """This class has a list"""
     def print_sorted(self):
         """This method prints the list but in ascending order"""
-        sorted_list = sorted(self)
-        filtered_list = [i for i in sorted_list if isinstance(i, int)]
-        print(filtered_list)
+        print(sorted(self))

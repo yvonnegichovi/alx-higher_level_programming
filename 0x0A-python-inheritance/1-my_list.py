90de58cf0b13ce5dd2e3828e2d93631ce869d89c
yvonnegichovi <yvonnegichovi@gmail.com>
2023-12-04 08:10:33 -0800

    a class that inherits

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""This module has a class MyList that inherits from list"""
+
+
+class MyList(list):
+    """This class has a list"""
+    def print_sorted(self):
+        """This method prints the list but in ascending order"""
+        print(sorted(self))

9e4400d8f693d2b08d90ec7639e5a4ff3084884e
yvonnegichovi <yvonnegichovi@gmail.com>
2023-12-05 09:57:46 -0800

    using dumps

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import json
+"""This model Returns the JSON representation of an object"""
+
+def to_json_string(my_obj):
+    """Returns tthe JSON representation of an objects(string)"""
+    json.dumps(my_obj)

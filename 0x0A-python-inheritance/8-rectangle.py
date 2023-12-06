987e2fab84137bdd89f771642792fdbf0ddf2c3d
yvonnegichovi <yvonnegichovi@gmail.com>
2023-12-06 10:52:36 -0800

    adding initialization

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -22,6 +22,8 @@ class Rectangle(BaseGeometry):
     """A class that inherits form the class BaseGeometry"""
     def __init__(self, width, height):
         """Instantiation of width and height using method init"""
+        self.__width = 0
+        self.__height = 0
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__height = height

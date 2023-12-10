604769a6539915cca12a9e63361225ae3484d865
yvonnegichovi <yvonnegichovi@gmail.com>
2023-12-10 04:31:16 -0800

    using args to each attribute

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,3 +91,17 @@ class Rectangle(Base):
     def __str__(self):
         """returns str of the rectangle"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args):
+        """This method assigns an argument to each attribute"""
+        num_args = len(args)
+        if num_args >= 1:
+            self.id = args[0]
+        if num_args >= 2:
+            self.__width = args[1]
+        if num_args >= 3:
+            self.__height = args[2]
+        if num_args >= 4:
+            self.__x = args[3]
+        if num_args >= 5:
+            self.__y = args[4]

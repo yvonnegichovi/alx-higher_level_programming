8a3f78980682e40160db6330d25f170347e607a2
yvonnegichovi <yvonnegichovi@gmail.com>
2023-12-10 02:58:37 -0800

    consider x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,9 +83,11 @@ class Rectangle(Base):
 
     def display(self):
         """Prints in stdot the Rectangle with character #"""
+        for i in range(self.__y):
+            print()
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
-        """prints str of the rectangle"""
-        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height))
+        """returns str of the rectangle"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)

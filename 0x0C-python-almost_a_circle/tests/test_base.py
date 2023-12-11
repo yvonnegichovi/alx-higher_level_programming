2112b09448e2c68d50c099c1629da4f6afd5108d
yvonnegichovi <yvonnegichovi@gmail.com>
2023-12-11 10:26:43 -0800

    tests/

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -57,41 +57,6 @@ class TestBase_Constructor(unittest.TestCase):
     def test_id_dict(self):
         self.assertEqual({"a": 0, "b": 1}, Base({"a": 0, "b": 1}).id)
 
-    def test_rectangle_case(self):
-        r1 = Rectangle(10, 2, 0, 0, 12)
-        self.assertIsInstance(r1, Rectangle)
-        self.assertTrue(hasattr(r1, 'id'))
-        self.assertEqual(r1.id, 12)
-        self.assertTrue(hasattr(r1, 'width'))
-        self.assertEqual(r1.width, 10)
-        self.assertTrue(hasattr(r1, 'height'))
-        self.assertEqual(r1.height, 2)
-        self.assertTrue(hasattr(r1, 'x'))
-        self.assertEqual(r1.x, 0)
-        self.assertTrue(hasattr(r1, 'y'))
-        self.assertEqual(r1.y, 0)
-
-    def test_invalid_height_type(self):
-        with self.assertRaises(TypeError) as context:
-            Rectangle(10, "2")
-        self.assertEqual(str(context.exception), "height must be an integer")
-
-    def test_invalid_width_value(self):
-        r = Rectangle(10, 2)
-        with self.assertRaises(ValueError) as context:
-            r.width = -10
-        self.assertEqual(str(context.exception), "width must be > 0")
-
-    def test_invalid_x_type(self):
-        r = Rectangle(10, 2)
-        with self.assertRaises(TypeError) as context:
-            r.x = {}
-        self.assertEqual(str(context.exception), "x must be an integer")
-
-    def test_invalid_y_value(self):
-        with self.assertRaises(ValueError) as context:
-            Rectangle(10, 2, 3, -1)
-        self.assertEqual(str(context.exception), "y must be >= 0")
 
 if __name__ == '__main__':
     unittest.main()
---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Unittest for rectangle.py
+Unittest classes:
+    TestBase_Constructor - line 14
+    """
+
+import unittest
+import os
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square

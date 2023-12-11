67994c8b25f6d56bb6309eeac86ce4a61daabbb9
yvonnegichovi <yvonnegichovi@gmail.com>
2023-12-11 07:28:03 -0800

    str

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,8 +90,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """returns str of the rectangle"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format
-        (self.id, self.__x, self.__y, self.__width, self.__height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
         """This method assigns an argument to each attribute"""

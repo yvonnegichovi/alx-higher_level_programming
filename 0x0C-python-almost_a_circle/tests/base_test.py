32c9df31e2830951f1a1aa79c3fa07ff47336907
yvonnegichovi <yvonnegichovi@gmail.com>
2023-12-11 09:18:43 -0800

    tests

--- 0x0C-python-almost_a_circle/tests/base_test.py
+++ 0x0C-python-almost_a_circle/tests/base_test.py
@@ -22,8 +22,37 @@ class TestBase(unittest.TestCase):
         b3 = Base()
         self.assertEqual(b1.id, b3.id - 2)
 
+    def test_two_id(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
+
     def test_custom_id_assignment(self):
         self.assertEqual(12, Base(12).id)
+
+    def test_id_str(self):
+        self.assertEqual("Holberton", Base("Holberton").id)
+
+    def test_id_bool(self):
+        self.assertEqual(True, Base(True).id)
+
+    def test_id_int(self):
+        self.assertEqual(6, Base(6).id)
+
+    def test_id_float(self):
+        self.assertEqual(6.0, Base(6.0).id)
+
+    def test_id_tuple(self):
+        self.assertEqual((1, 2, 3), Base((1, 2, 3)).id)
+
+    def test_id_list(self):
+        self.assertEqual([1, 2, 3], Base([1, 2, 3]).id)
+
+    def test_id_set(self):
+        self.assertEqual({1, 2, 3}, Base({1, 2, 3}).id)
+
+    def test_id_dict(self):
+        self.assertEqual({"a": 0, "b": 1}, Base({"a": 0, "b": 1}).id)
 
     def test_rectangle_case(self):
         r1 = Rectangle(10, 2, 0, 0, 12)

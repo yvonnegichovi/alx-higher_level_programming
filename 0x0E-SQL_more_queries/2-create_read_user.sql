d141aac802b15e608f930ff4f3e9d94620ffdfc1
yvonnegichovi <yvonnegichovi@gmail.com>
2024-01-17 11:51:42 -0800

    create user grant access

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- Create or update database hbtn_0d_2 and user_0d_2 with all privileges
+-- Grant all privileges to user_0d_2
+-- Flush privileges to apply changes
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;

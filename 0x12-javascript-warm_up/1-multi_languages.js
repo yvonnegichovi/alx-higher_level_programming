4a8a1ac76d63cd77b178e593313e4d0fcd41868f
yvonnegichovi <yvonnegichovi@gmail.com>
2024-02-12 10:12:57 -0800

    Task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let line1 = "C is fun";
-let line2 = "Python is cool";
-let line3 = "JavaScript is amazing";
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
 console.log(line1);
 console.log(line2);
 console.log(line3);

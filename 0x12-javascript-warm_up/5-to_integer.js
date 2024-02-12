d13ce5eda81848cff5abe2efc38eef891762e46b
yvonnegichovi <yvonnegichovi@gmail.com>
2024-02-12 13:32:35 -0800

    Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const arg = process.argv[2];
 const num = parseInt(arg);
 if (!isNaN(num) && Number.isInteger(num)) {
-	console.log('My number:', num);
+  console.log('My number:', num);
 } else {
-	console.log("Not a number");
+  console.log('Not a number');
 }

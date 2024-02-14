87342fbf7ed75ae29c99718b44cf97cd0dc2622e
yvonnegichovi <yvonnegichovi@gmail.com>
2024-02-14 10:39:51 -0800

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,13 +3,9 @@
 const BaseSquare = require('./5-square');
 
 class Square extends BaseSquare {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint(c) {
     if (c === undefined) {
-      c = 'X';
+      super.print();
     }
     for (let i = 0; i < this.size; i++) {
       console.log(c.repeat(this.size));

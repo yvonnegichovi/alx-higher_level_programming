6a9e7a116f6e44be8238f5c53a1c71a4fa880acf
yvonnegichovi <yvonnegichovi@gmail.com>
2024-02-14 12:11:09 -0800

    Task 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const length = list.length;
+  for (let i = 0; i < Math.floor(length / 2); i++) {
+    const temp = list[i];
+    list[i] = list[length - 1 - i];
+    list[length - 1 - i] = temp;
+  }
+  return list;
+}

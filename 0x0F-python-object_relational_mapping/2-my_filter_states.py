1b6d8a862a777c1bd645f0b5c98d38a2feb06ae0
yvonnegichovi <yvonnegichovi@gmail.com>
2024-02-16 11:27:54 -0800

    Task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""This module lists all states starting with N
+from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """filters a lists of all states"""
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name = '{}'
+                ORDER BY states.id ASC""".format(sys.argv[4]))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()

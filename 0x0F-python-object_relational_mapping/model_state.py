e93a50272f5278568070b56d035307aab3cc4661
yvonnegichovi <yvonnegichovi@gmail.com>
2024-02-17 11:16:44 -0800

    TASK 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,6 +5,7 @@ contains the class definition of a State and an instance
 Base = declarative_base()
 """
 
+import sys
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -17,5 +18,12 @@ class State(Base):
     linke to the MYYSQL table states
     """
     __tablename__ = 'states'
-    id = Column(Integer, NOT NULL, primary_key=True)
-    name = Column(String(128), NOT NULL)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+if __name__ == "__main__":
+    from sqllalchemey  import create_engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                            pool_pre_ping=True)
+    Base.metadata.create_all(engine)

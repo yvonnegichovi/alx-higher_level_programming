4653aa27269ed8380940003f6d94391f467698f3
yvonnegichovi <yvonnegichovi@gmail.com>
2024-02-17 12:35:33 -0800

    TASK 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,9 +10,9 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     """create engine"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                            sys.argv[1], sys.argv[2], sys.argv[3]),
-                            pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     """Create a configered'Session' class"""
     Session = sessionmaker(bind=engine)
     """Create a Session"""

974e360ef668a44982cdeba6c9ae32a5810f4f77
yvonnegichovi <yvonnegichovi@gmail.com>
2024-02-17 11:34:42 -0800

    TASK 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -20,10 +20,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-if __name__ == "__main__":
-    from sqllalchemey  import create_engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)

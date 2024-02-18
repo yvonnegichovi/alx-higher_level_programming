3a4466a46960c96cbcca3b514f04a7ef05fe337e
yvonnegichovi <yvonnegichovi@gmail.com>
2024-02-18 02:11:16 -0800

    Task 11

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     """Create a configered'Session' class"""
     Session = sessionmaker(bind=engine)
     """Create a Session"""

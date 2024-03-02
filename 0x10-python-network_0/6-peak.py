48b009e0490b8a9609ae843936fd9859a355892e
yvonnegichovi <yvonnegichovi@gmail.com>
2024-03-02 03:11:15 -0800

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,10 +4,10 @@
 
 def find_peak(list_of_integers):
     """Prototype for this sorting"""
-    max = list_of_integers[0]
     if len(list_of_integers) is 0:
         return None
     else:
+        max = list_of_integers[0]
         for i in list_of_integers:
             if max < list_of_integers[i]:
                 list_of_integers[i] = max

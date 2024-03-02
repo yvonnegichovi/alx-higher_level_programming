177212d899e248c529a45df86a1babef5dfe863f
yvonnegichovi <yvonnegichovi@gmail.com>
2024-03-02 02:49:58 -0800

    5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # returns POST request with custom header
-curl -s -X POST -d "email=test@gmail.com&subject=will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"

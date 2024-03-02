086427abd79d3ba6319f1c58fd999f0b1108a43e
yvonnegichovi <yvonnegichovi@gmail.com>
2024-03-02 01:13:02 -0800

    1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL and displays the body request
-curl -s -o /dev/null -w "%{http_code}" "&1" | { read status_code; && [ "$status_code" -eq 200 ] && curl -s "$1"; }
+curl -sL "&1"

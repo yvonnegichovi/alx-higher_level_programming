24bc7b5203d617d4ff3998b9b953d5bbd2e126ef
yvonnegichovi <yvonnegichovi@gmail.com>
2024-03-02 12:44:41 -0800

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,4 +10,4 @@ with urllib.request.urlopen(url) as response:
     print("Body response:")
     print("\t- type:", type(body))
     print("\t- content:", body)
-    print("\t- utf8 content", body.decode("utf-8"))
+    print("\t- utf8 content:", body.decode("utf-8"))

507fb538c529abd7ff4233d5bb64db5ace8a663b
yvonnegichovi <yvonnegichovi@gmail.com>
2024-03-03 06:52:22 -0800

    2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,8 +8,8 @@ import sys
 
 if __name__ == "__main__":
     """Ensures that the code is not executed when imported"""
-    url =  sys.argv[1]
-    email =  sys.argv[2]
+    url = sys.argv[1]
+    email = sys.argv[2]
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
     req = urllib.request.Request(url, data=data, method='POST')
     with urllib.request.urlopen(req) as response:

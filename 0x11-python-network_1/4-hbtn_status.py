83b3e1492ba67d5cd3ab15c075d18f6e94081e2c
yvonnegichovi <yvonnegichovi@gmail.com>
2024-03-03 09:05:30 -0800

    4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""fetches an URL using requests package only"""
+
+import requests
+
+
+if __name__ == "__main__":
+    """This ensures that the code is not executed when imported"""
+    url = "https://alx-intranet.hbtn.io/status"
+    body = requests.get(url)
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)

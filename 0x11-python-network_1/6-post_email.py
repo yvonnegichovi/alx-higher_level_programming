128642d7d787ae116545ab5e6f4951d9c455f027
yvonnegichovi <yvonnegichovi@gmail.com>
2024-03-03 09:42:30 -0800

    6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""sends a POST requests to the URL passed witth an email as a parameter"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    """Ensures that the code is not executed when imported"""
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {'email': email}
+    body = requests.post(url, data=payload)
+    print("Your email is:", email)

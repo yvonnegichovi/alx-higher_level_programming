7ed559057da17924b8774a6a2561101eb36eb791
yvonnegichovi <yvonnegichovi@gmail.com>
2024-04-04 21:05:43 +0300

    task 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $.ajax({
+        url: "https://swapi-api.alx-tools.com/api/films/?format=json",
+        method: "GET",
+        success: function(data) {
+            $.each(data.results, function(index, movie) {
+                $("<li>").text(movie.title).appendTo("#list_movies");
+            });
+        },
+        error: function() {
+            $("#list_movies").append("<li>Error fetching movies</li>");
+        }
+    });
+});

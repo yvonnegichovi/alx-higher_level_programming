58112908db985baa7bed36033fc6b62c99cc9e0d
yvonnegichovi <yvonnegichovi@gmail.com>
2024-04-04 22:38:44 +0300

    task 1.2

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,10 +3,6 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-if (!filePath || !content) {
-  console.error('Please provide both the file path and the content.');
-  process.exit(1);
-}
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
     console.error(err);

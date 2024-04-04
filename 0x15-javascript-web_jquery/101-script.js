245dd5201cf353deed696955868705c44780abdb
yvonnegichovi <yvonnegichovi@gmail.com>
2024-04-04 21:36:51 +0300

    task 11

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#add_item").click(function() {
+        $("ul.my_list").append("Item");
+   });
+    $("#remove_item").click(function() {
+        $("ul.my_list li:last-child").remove();
+   });
+    $("#clear_list").click(function() {
+        $("ul.my_list").empty();
+    });
+});

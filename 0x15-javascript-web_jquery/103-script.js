0f64f8d153aea464be5943ce1b38797b41687169
yvonnegichovi <yvonnegichovi@gmail.com>
2024-04-04 21:50:21 +0300

    task 13

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#btn_translate').click(function() {
+    function fetchTranslation() {
         const languageCode = $('#language_code').val();
         const url = `https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`;
 
@@ -9,5 +9,13 @@ $(document).ready(function() {
             console.error('Error:', errorThrown);
             $('#hello').text('Error: Failed to fetch translation');
         });
+    }
+
+    $('#btn_translate').click(fetchTranslation);
+
+    $('#language_code').keypress(function(event) {
+        if (event.which === 13) {
+            fetchTranslation();
+        }
     });
 });

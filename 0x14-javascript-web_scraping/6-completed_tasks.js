677220bc603a2bb51918b3981d0f3c129e532b51
yvonnegichovi <yvonnegichovi@gmail.com>
2024-04-07 22:00:33 +0300

    task 6.1

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,34 +1,34 @@
 #!/usr/bin/node
 const request = require('request');
 
-function countCompletedTasks(apiUrl) {
-    request.get(apiUrl, (error, response, body) => {
-        if (error) {
-            console.error('Error:', error);
-            return;
+function countCompletedTasks (apiUrl) {
+  request.get(apiUrl, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      return;
+    }
+
+    if (response.statusCode === 200) {
+      const todos = JSON.parse(body);
+
+      const completedTasksByUser = {};
+
+      todos.forEach(todo => {
+        if (todo.completed) {
+          if (completedTasksByUser[todo.userId]) {
+            completedTasksByUser[todo.userId]++;
+          } else {
+            completedTasksByUser[todo.userId] = 1;
+          }
         }
+      });
 
-        if (response.statusCode === 200) {
-            const todos = JSON.parse(body);
-
-            const completedTasksByUser = {};
-
-            todos.forEach(todo => {
-                if (todo.completed) {
-                    if (completedTasksByUser[todo.userId]) {
-                        completedTasksByUser[todo.userId]++;
-                    } else {
-                        completedTasksByUser[todo.userId] = 1;
-                    }
-                }
-            });
-
-            // Printing the result
-            console.log(completedTasksByUser);
-        } else {
-            console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
-        }
-    });
+      // Printing the result
+      console.log(completedTasksByUser);
+    } else {
+      console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
+    }
+  });
 }
 
 // Retrieving command line arguments
@@ -36,8 +36,8 @@ const apiUrl = process.argv[2];
 
 // Checking if the API URL is provided as argument
 if (!apiUrl) {
-    console.error('Usage: node 6-completed_tasks.js <API_URL>');
+  console.error('Usage: node 6-completed_tasks.js <API_URL>');
 } else {
-    // Calling the function to compute completed tasks by user id
-    countCompletedTasks(apiUrl);
+  // Calling the function to compute completed tasks by user id
+  countCompletedTasks(apiUrl);
 }

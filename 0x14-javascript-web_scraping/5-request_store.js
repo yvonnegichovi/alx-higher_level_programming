f20511ffd258ddb1723f310ca88165046c1a34c0
yvonnegichovi <yvonnegichovi@gmail.com>
2024-04-07 21:53:17 +0300

    task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+
+const [, , url, filePath] = process.argv;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Failed to get data. Status code:', response.statusCode);
+    return;
+  }
+
+  fs.writeFile(filePath, body, { encoding: 'utf-8' }, err => {
+    if (err) {
+      console.error('Error writing to file:', err);
+      return;
+    }
+    console.log('File has been saved successfully.');
+  });
+});
